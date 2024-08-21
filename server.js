b88844ad3485b09188e9d64ab3912e4447c26ef7
Prof edir <ecaramba@gmail.com>
2024-08-20 21:30:52 -0300

    enviando email

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
+const nodemailer = require("nodemailer");
+
+/* const config = {
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: true,
+    auth: {
+        user: "seuemail@gmail.com",
+        pass: "senhasupersecreta"
+    }
+}; */
+const config = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'nikolas.boyle88@ethereal.email',
+        pass: '1DMWQZBhvafpVWU21P'
+    }
+}; 
+const servidor = nodemailer.createTransport(config);
+
 // habilita ler form
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,10 +44,24 @@ app.post("/contato", function(req, res){
     let linha = req.body.nome +","+req.body.email + "\n";
     let opt = {flag: "a"};
 
-    fs.writeFile("lista.csv", linha, opt, function(erro){
+    // fs.writeFile("lista.csv", linha, opt, function(erro){
         
-        res.send("salvo");
+    //     res.send("salvo");
 
+    // });
+
+    let conteudo = "<p>Nome: " + req.body.nome +"</p>"
+            + "<p> Email: " + req.body.email + "</p>";
+
+    let email = {
+        from: "edir@prof.com",
+        to: "jhenni.bueno@gmail.com",
+        subject: "teste de email",
+        html: conteudo
+    };
+            
+    servidor.sendMail(email, function(){
+        res.send("email enviado");
     });
     
 });
