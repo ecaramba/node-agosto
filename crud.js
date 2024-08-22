dbc686e1aa7825c44f63972ab28baef7e59ac4ab
Prof edir <ecaramba@gmail.com>
2024-08-22 20:06:29 -0300

    crud

--- crud.js
+++ crud.js
@@ -4,4 +4,21 @@
 // Update -> atualização
 // Delete -> apagar
 
+const { MongoClient } = require("mongodb");
+
+const urlcon = "mongodb+srv://aula-back:aula123456@turma-agosto.mam4s.mongodb.net/";
+
+const conexao = new MongoClient(urlcon);
+
+async function lerDados()
+{
+    let db = await conexao.connect();  
+    let pasta = db.db("edir").collection("contatos");  
+    let dados = await pasta.find({}).toArray();
+
+    console.log(dados);
+}
+
+lerDados();
+
 
